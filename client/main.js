a02987cb2f943d125d5d4d39770b3636dbfc34df
just-me- <hess-marcel@bluewin.ch>
2017-08-19 22:20:10 +0200

    Sicherung Zwischenstand - gescheitert

--- client/main.js
+++ client/main.js
@@ -3,6 +3,21 @@ import angularMeteor from 'angular-meteor';
 import todosList from '../imports/components/todosList/todosList';
 import '../imports/startup/accounts-config.js';
 
+
+Meteor.startup(function(){
+
+  Router.route('/home', function () {
+    this.render('homeTemplate');
+  });
+
+  /*
+  Router.configure({
+    notFoundTemplate: "404_notfound"
+  });
+  */
+
+});
+
 angular.module('simple-todos', [
   angularMeteor,
   todosList.name,
