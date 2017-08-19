a600cf265be30ca1cea1adaa575c5c0aa5de3fdd
just-me- <hess-marcel@bluewin.ch>
2017-08-19 23:10:50 +0200

    Add route :)

--- client/main.js
+++ client/main.js
@@ -1,2 +1,16 @@
 import '../imports/startup/accounts-config.js';
 import '../imports/ui/body.js';
+
+Meteor.startup(function(){
+
+  Router.route('/home', function () {
+    this.render('homeTemplate');
+  });
+
+  /*
+  Router.configure({
+    notFoundTemplate: "404_notfound"
+  });
+  */
+
+});
