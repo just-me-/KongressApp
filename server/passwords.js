de918222863b416361113f4520a3497d0708fe32
just-me- <hess-marcel@bluewin.ch>
2017-09-11 14:20:10 +0200

    Keynote is now an admin thing

--- server/passwords.js
+++ server/passwords.js
@@ -6,7 +6,7 @@ var serverOnly_modPassword       = "12";
 
 // Protecting admin and mod urls
 var basicAuthAdmin = new HttpBasicAuth(serverOnly_adminUser, serverOnly_adminPassword);
-basicAuthAdmin.protect(['/admin_program', '/admin_speaker', '/admin_sponsor', 'admin_room']);
+basicAuthAdmin.protect(['/admin_program', '/admin_speaker', '/admin_sponsor', '/admin_room', '/keynote']);
 
 var basicAuthMod = new HttpBasicAuth(serverOnly_modUser, serverOnly_modPassword);
-basicAuthMod.protect(['/keynote', '/keynote_mod', '/keynote_answers']);
+basicAuthMod.protect(['/keynote_mod', '/keynote_answers']);
